f2a3da7ded611c986e7652a046961e8e93463ea5
Daniele Campari <danielecampari@gmail.com>
2023-07-06 18:25:02 +0200

    unify jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   moduleFileExtension: ['js', 'json'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+      '<rootDir>/src/test/mocks/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/src/test/mocks/styleMock.js'
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest'
   }
 };
