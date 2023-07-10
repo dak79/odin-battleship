f3a2ccf7c76645c94d1779b2ef438b1c30ec4e93
Daniele Campari <danielecampari@gmail.com>
2023-07-10 18:19:40 +0200

    add some todo to explore gameBoard functionality

---
+++ src/test/gameBoard.test.js
@@ -0,0 +1,19 @@
+import { GameBoard } from '../components/gameBoard';
+
+describe('GameBoard', () => {
+  it.todo('has 10 column');
+  it.todo('has 10 row');
+  it.todo('index in boudary');
+  it.todo('index out of boudary');
+});
+
+describe('GameBoard is', () => {
+  it.todo('able to create a ship');
+});
+
+describe('Placing ship', () => {
+  it.todo('place a ship inside boudary');
+  it.todo('there are 4 types of ship 4, 3, 2, 1');
+  it.todo('reverse change the ship orientation in vertical/orizontal');
+  it.todo('does not place a ship out of boudary');
+});
