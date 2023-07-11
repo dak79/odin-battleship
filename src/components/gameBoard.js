d2f685edf301cb2703d1442357661a454db62a39
Daniele Campari <danielecampari@gmail.com>
2023-07-11 18:50:09 +0200

    add analysis on placeship

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,3 +1,5 @@
+import { Ship } from './ships';
+
 export const GameBoard = () => {
   const init = () => {
     const board = new Map();
@@ -10,7 +12,37 @@ export const GameBoard = () => {
     return board;
   };
 
+  const createShip = (length) => Ship(length);
+
+  const placeShip = (shipCenter, ship, board) => {
+    let x = 0;
+    let y = 0;
+    const shipModel = [
+      [x, y - 2],
+      [x, y - 1],
+      [x, y],
+      [x, y + 1]
+    ];
+
+    let square = shipCenter.toString();
+    let [a, b] = shipCenter;
+    console.log(a);
+    console.log(b);
+
+    const shipCoord = [];
+    shipModel.forEach((s) => {
+      shipCoord.push([s[0] + a, s[1] + b]);
+    });
+
+    console.log(shipCoord);
+    // TODO: ORA CHE ABBIAMO SHIP COORD ITERIAMO: prediamo le prime coordinate
+    // accediamo a board quelle coordinate, facciamo un push di tutte le altre
+    // coordinate che non siano quelle usate per accedere all'array coordinate
+    // board. Questo per tutte le coordiante nell'array.
+  };
   return {
-    init
+    init,
+    createShip,
+    placeShip
   };
 };
--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -31,15 +31,49 @@ describe('GameBoard', () => {
   it('has not square [4, 11]', () => {
     expect(board.get('4,11')).toBeUndefined();
   });
-});
 
-describe('Create a ship', () => {
-  it.todo('able to create a ship');
+  it('has createShip method', () => {
+    expect(b).toHaveProperty('createShip');
+  });
+
+  it('create a length 4 ship', () => {
+    const ship = b.createShip(4);
+    expect(ship.getLength()).toBe(4);
+  });
+
+  it('create a length 3 ship', () => {
+    const ship = b.createShip(3);
+    expect(ship.getLength()).toBe(3);
+  });
+
+  it('create a length 2 ship', () => {
+    const ship = b.createShip(2);
+    expect(ship.getLength()).toBe(2);
+  });
+
+  it('create a length 1 ship', () => {
+    const ship = b.createShip(1);
+    expect(ship.getLength()).toBe(1);
+  });
+
+  it('has the method placeShip', () => {
+    expect(b).toHaveProperty('placeShip');
+  });
+
+  it('place a ship of length 4 in [0, 0][0, 1][0, 2][0, 3]', () => {
+    const ship = b.createShip(4);
+    b.placeShip([0, 2], ship, board);
+    expect(board.get('0,0')[0]).toBe([0, 1]);
+    expect(board.get('0,1')[0]).toBe([0, 2]);
+    expect(board.get('0,2')[0]).toBe([0, 3]);
+    expect(board.get('0,3')[0]).toBe([0, 2]);
+  });
 });
 
 describe('Placing ship', () => {
   it.todo('place a ship inside boudary');
+  it.todo('does not place a ship where there is another ship');
+  it.todo('does not place a ship out of boudary');
   it.todo('there are 4 types of ship 4, 3, 2, 1');
   it.todo('reverse change the ship orientation in vertical/orizontal');
-  it.todo('does not place a ship out of boudary');
 });
