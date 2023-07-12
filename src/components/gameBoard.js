a008e3eef23e4644255887d9de8b4f069059b93c
Daniele Campari <danielecampari@gmail.com>
2023-07-12 23:02:00 +0200

    refract code and fix comment

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -25,29 +25,29 @@ export const GameBoard = () => {
   const createShip = (length) => Ship(length);
 
   /**
-   * Define if a ship is placed horizontally or vertically
+   * Change ship orientation: vertical / horizontal.
    * @param {Boolean} isVertical
    * @param {Number} x
    * @param {Number} y
-   * @returns Model for vertical/horizontal ship.
+   * @returns {Function} For generating vertical or horizontal ship model
    */
   const shipOrientation = (isVertical, x = 0, y = 0) =>
     isVertical ? (_, i) => [x, y + i] : (_, i) => [x + i, y];
 
   /**
-   * Create coordinates for ship according to its length
+   * Create a model for ship
    * @param {Number} shipLength
    * @param {Boolean} orientation - true vertical, false horizontal
-   * @returns An array of coordinate of a ship of given length
+   * @returns {Number[][]} A vertical/horizontal ship model.
    */
   const shipModel = (shipLength, orientation) =>
     Array.from({ length: shipLength }, orientation);
 
   /**
-   * Get the coordinate model for the ship
+   * Get the model according to ship length and orientation
    * @param {Number} shipLength
    * @param {Boolean} isVertical
-   * @returns An array.
+   * @returns {Number[][]} - Get the ship model.
    */
   const getShipModel = (shipLength, isVertical) =>
     pipe(
@@ -56,18 +56,62 @@ export const GameBoard = () => {
     )(shipLength);
 
   /**
-   * Attualize the coordinate of ship starting from ship head
-   * @param {Array} param - destructure coordinate of ship head.
-   * @param {Array[Array]} model - Ship model
-   * @returns
+   * Use the model for generating ship coordinate.
+   * @param {Number[]} param - destructure coordinate of ship head.
+   * @param {Number[][]} model - Ship model
+   * @returns {Number[][]} - Update the coordinate by passing the starting point
    */
   const shipCoord = ([x, y], model) =>
     model.map(([dx, dy]) => [dx + x, dy + y]);
 
   /**
+   * Get the coordinate of ship
+   * @param {Number[]} shipHead
+   * @param {Number} shipLength
+   * @param {Boolean} orientation
+   * @returns {Number[][]} - Coordinate of ship
+   */
+  const getShipCoord = (shipHead, shipLength, orientation) =>
+    pipe(
+      (length) => getShipModel(length, orientation),
+      (model) => shipCoord(shipHead, model)
+    )(shipLength);
+
+  /**
+   * Check if the coordinates of ship are on board
+   * @param {Number[][]} coord
+   * @returns {Boolean}
+   */
+  const isShipInBoard = (coords) =>
+    !coords.some(([x, y]) => x < 0 || x > 9 || y < 0 || y > 9);
+
+  /**
+   * If ship is on board, check if the space is available for a ship
+   * @param {Number[][]} coords - New ship coordinate
+   * @param {Map} board
+   * @returns {Boolean}
+   */
+  const isSpaceEmpty = (onBoard, coords, board) =>
+    onBoard
+      ? ![...coords].some((coord) => board.get(coord.toString()).length !== 0)
+      : onBoard;
+
+  /**
+   * Check if it is a valid placing
+   * @param {Number[][]} shipMod - Ship coordinate
+   * @param {Map} board
+   * @returns {Boolean}
+   */
+  const isValidShipPlacement = (shipMod, board) =>
+    pipe(
+      () => isShipInBoard(shipMod),
+      (onBoard) => isSpaceEmpty(onBoard, shipMod, board)
+    )();
+
+  /**
    * Add ship on board graph
    * @param {Map} board - Map representing the board
-   * @param {Array[Array]} coords - Array of coordinates
+   * @param {Number[][]} coords - Array of coordinates
    * @returns
    */
   const addToBoard = (board, coords) =>
@@ -78,43 +122,25 @@ export const GameBoard = () => {
     );
 
   /**
-   * Check if the coordinates of ship are on board
-   * @param {Array} array
-   * @returns true|false
-   */
-  const isShipInBoard = (array) =>
-    !array.some(([x, y]) => x < 0 || x > 9 || y < 0 || y > 9);
-
-  /**
-   * Check if the space is available for a ship
-   * @param {Array} array - New ship coordinate
-   * @param {Map} board
-   * @returns
-   */
-  const isSpaceEmpty = (onBoard, array, board) =>
-    onBoard
-      ? ![...array].some((coord) => board.get(coord.toString()).length !== 0)
-      : onBoard;
-
-  const isValidShipPlacement = (array, board) =>
-    pipe(
-      () => isShipInBoard(array),
-      (onBoard) => isSpaceEmpty(onBoard, array, board)
-    )();
-  /**
-   * Place ship on board
-   * @param {Array} shipHead - Coordinate of ship head.
+   * Try the placement of ship
+   * @param {Number[]} shipHead - Coordinate of ship head.
    * @param {Number} shipLength
    * @param {Map} board
-   * @returns
+   * @returns {Number[][]|false}
    */
-  const placeShip = (shipHead, shipLength, board, orientation) =>
-    pipe(
-      (length) => getShipModel(length, orientation),
-      (model) => shipCoord(shipHead, model),
-      (coords) => addToBoard(board, coords)
+  const placeShip = (shipHead, shipLength, board, orientation) => {
+    const isACorrectDeploy = pipe(
+      (length) => getShipCoord(shipHead, length, orientation),
+      (coords) => isValidShipPlacement(coords, board)
     )(shipLength);
 
+    return isACorrectDeploy
+      ? pipe(
+          (length) => getShipCoord(shipHead, length, orientation),
+          (coords) => addToBoard(board, coords)
+        )(shipLength)
+      : false;
+  };
   return {
     init,
     createShip,
