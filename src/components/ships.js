ef0e32cfd56b34c079fbdaf5de0a395c6025631a
Daniele Campari <danielecampari@gmail.com>
2023-07-13 15:55:41 +0200

    refract ship and ship.test

--- src/components/ships.js
+++ src/components/ships.js
@@ -1,49 +1,65 @@
 import { pipe } from '../util/pipe';
-export const Ship = (l) => {
+export const Ship = (len) => {
   /**
    * Check if the input is a string that can be parsed in a valid integer.
    * @param {*} input - length
-   * @returns input|input typeof number.
+   * @returns {*} input
    */
-  const parseLength = (input = l) =>
+  const parseLength = (input) =>
     typeof input === 'string' ? parseInt(input) : input;
 
   /**
    * Check if input is a valid integer.
    * @param {*} input - length
-   * @returns integer|false
+   * @returns {Number|false}
    */
   const validateLength = (input) => (Number.isFinite(input) ? input : false);
 
   /**
    * Check if the length is between 1 and 4.
-   * @param {*} input - length
-   * @returns input|false
+   * @param {Number|false} input - length
+   * @returns {Number|false}
    */
   const validShip = (input) => (input > 4 || input < 2 ? false : input);
 
   /**
    * Get the length of ship.
    */
-  const getLength = pipe(parseLength, validateLength, validShip);
+  const getLength = () =>
+    pipe(
+      () => parseLength(len),
+      (parsed) => validateLength(parsed),
+      (int) => validShip(int)
+    )(len);
 
   const length = getLength();
 
   let hits = 0;
 
+  let sunk = false;
+
   /**
-   * Hit the ship
-   * @returns increase number of hit by 0ne
+   * Increment hits on a ship.
+   * @returns {Number}
    */
-  const hit = () => (hits += 1);
+  const addHit = () => hits++;
+
+  /**
+   * setSunk state
+   * @returns {Boolean} New value for variable sunk
+   *
+   */
+  const setSunk = () => (hits === length ? (sunk = true) : (sunk = false));
+
+  /**
+   * Hit a ship and set sunk state.
+   */
+  const hit = pipe(addHit, setSunk);
 
   /**
    * Check if the ship is sunked.
-   * @param {Number} damage - Quantity of hits
-   * @param {Number} health - Length of ship
-   * @returns
    */
-  const isSunk = (damage = hits, health = length) => damage === health;
+  const isSunk = () => sunk;
 
   return {
     getLength,
--- src/test/ships.test.js
+++ src/test/ships.test.js
@@ -1,24 +1,38 @@
 import { Ship } from '../components/ships.js';
 
-describe('Ships', () => {
-  it('must have a length', () => {
+const shipTwo = Ship(2);
+const shipThree = Ship('3');
+const shipFour = Ship(4);
+
+describe('Ship', () => {
+  it('has the method .getLength()', () => {
+    expect(shipTwo).toHaveProperty('getLength');
+  });
+
+  it('has method .hit()', () => {
+    expect(shipThree).toHaveProperty('hit');
+  });
+
+  it('has method .isSunk()', () => {
+    expect(shipFour).toHaveProperty('isSunk');
+  });
+});
+
+describe('The .getLength() method', () => {
+  it('returns false if argument in factory is missed', () => {
     const ship = Ship();
     expect(ship.getLength()).toBe(false);
   });
 
-  it('length must be only an integer', () => {
-    const int = Ship(2);
+  it('returns false if argument is not an integer or a parsed string', () => {
     const str = Ship('n');
-    const strInt = Ship('2');
     const obj = Ship({});
     const arr = Ship([]);
     const bool = Ship(true);
     const nan = Ship(NaN);
     const und = Ship(undefined);
     const nu = Ship(null);
-    expect(int.getLength()).toBe(2);
     expect(str.getLength()).toBe(false);
-    expect(strInt.getLength()).toBe(2);
     expect(obj.getLength()).toBe(false);
     expect(arr.getLength()).toBe(false);
     expect(bool.getLength()).toBe(false);
@@ -27,12 +41,9 @@ describe('Ships', () => {
     expect(nu.getLength()).toBe(false);
   });
 
-  it('can have length between 2 and 4', () => {
+  it('returns a value inside min and max length (between 2 and 4)', () => {
     const shipZero = Ship(0);
     const shipOne = Ship(1);
-    const shipTwo = Ship(2);
-    const shipThree = Ship(3);
-    const shipFour = Ship(4);
     const shipFive = Ship(5);
     expect(shipZero.getLength()).toBe(false);
     expect(shipOne.getLength()).toBe(false);
@@ -41,39 +52,33 @@ describe('Ships', () => {
     expect(shipFour.getLength()).toBe(4);
     expect(shipFive.getLength()).toBe(false);
   });
+});
 
-  it('is not sunked if there are no damage', () => {
-    const ship = Ship(4);
-    expect(ship.isSunk()).toBe(false);
+describe('The method .isSunk()', () => {
+  it('returns false, no damage on ship', () => {
+    expect(shipFour.isSunk()).toBe(false);
   });
 
-  it('has length 3 and is not sunked if it has been hit once', () => {
-    const ship = Ship(3);
-    ship.hit();
-    expect(ship.isSunk()).toBe(false);
+  it('returns false: it has been hit once and has length 4', () => {
+    shipFour.hit();
+    expect(shipThree.isSunk()).toBe(false);
   });
 
-  it('has length 3 and is not sunked if it has been hit twice', () => {
-    const ship = Ship(3);
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(false);
+  it('has length 3 and it is not sunked: it has been only hit twice', () => {
+    shipThree.hit();
+    shipThree.hit();
+    expect(shipThree.isSunk()).toBe(false);
   });
 
-  it('has length 3 and is sunked if it has been hit three times', () => {
-    const ship = Ship(3);
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
+  it('has length 3 and it is sunked: it has been hit three times', () => {
+    shipThree.hit();
+    expect(shipThree.isSunk()).toBe(true);
   });
 
-  it('has length 4 and is sunked if it has been hit four times', () => {
-    const ship = Ship(4);
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
+  it('has length 4 and it is sunked: it has been hit four times', () => {
+    shipFour.hit();
+    shipFour.hit();
+    shipFour.hit();
+    expect(shipFour.isSunk()).toBe(true);
   });
 });
