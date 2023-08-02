a1462e5d8cd2b3896f6846ee16b4ef5cae079073
Daniele Campari <danielecampari@gmail.com>
2023-08-02 13:38:34 +0200

    add todo for new feature

--- src/components/game.js
+++ src/components/game.js
@@ -144,4 +144,10 @@ const game = (() => {
 export default game;
 
 /* TODO:
+ * - Method to take input for attack from player 2 board.
+ *   - Apply an eventListener to all cells of cpu board
+ *   - Create an event handler which take the click and parse an x and an y
+ *    - Decide where to put event handler, maybe in a file or in game.
+ *    - Carefull about IEEF game, might need a new file or module not IEFF.
+ *      Maybe is better to separate handler and game IEEF.
  */
