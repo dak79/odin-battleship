24936d42974f5df4c4ef1512ba2c49588192646f
Daniele Campari <danielecampari@gmail.com>
2023-08-04 23:01:49 +0200

    add comments

--- src/dom/eventListeners.js
+++ src/dom/eventListeners.js
@@ -1,5 +1,9 @@
 import eventHandlers from './eventHandlers';
-
+/**
+ * Add click listener to cpu board.
+ * @param {HTMLElement} table
+ * @returns
+ */
 const clicksRivalBoard = (table) => {
   const cells = table.querySelectorAll('td');
 
@@ -10,21 +14,42 @@ const clicksRivalBoard = (table) => {
   return cells;
 };
 
+/**
+ * Remove click listener from cpu board
+ * @param {Array[]} cells
+ */
 const removeClicksRivalBoard = (cells) => {
   cells.forEach((cell) =>
     cell.removeEventListener('click', eventHandlers.parseAttackCoords)
   );
 };
+
+/**
+ * Remove listener from a cell.
+ * @param {Array[]} cell
+ * @returns
+ */
 const removeClickRivalBoard = (cell) =>
   cell.removeEventListener('click', eventHandlers.parseAttackCoords);
 
+/**
+ * Initialize event listeners
+ * @param {HTMLElement} body
+ */
 const initializeEventListeners = (body) => {
   const rivalTable = body.main.querySelector('#board-rival');
   const cellsRivalBoard = clicksRivalBoard(rivalTable);
 };
+
+/**
+ * Export event listeners
+ * @param {HTMLElement} body
+ * @returns
+ */
 const eventListeners = (body) => {
   return {
     events: () => initializeEventListeners(body)
   };
 };
+
 export default eventListeners;
