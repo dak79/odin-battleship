4a35e756ed2cd2e4bdd88e765bc9c669d97787fe
Daniele Campari <danielecampari@gmail.com>
2023-08-04 16:35:22 +0200

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style/style.css';
 import DOM from './dom/DOM';
+import eventListeners from './dom/eventListeners';
 import game from './components/game';
 
-//game;
-DOM();
+const hook = document.querySelector('#hook');
+const body = DOM(hook, game).render();
+
+eventListeners(body).events();
