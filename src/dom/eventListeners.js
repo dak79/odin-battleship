63a64a3ed42d8b298b77cd5df18d5387f2af52ae
Daniele Campari <danielecampari@gmail.com>
2023-08-17 16:11:16 +0200

    refactor code

--- src/dom/eventListeners.js
+++ src/dom/eventListeners.js
@@ -1,35 +1,24 @@
-import game from '../components/game';
+import game from '../components/game/game';
 import DOM from './DOM';
-// const btnStart = body.querySelector('#body-main #controllers #button-start');
-//   events.removeStartBtn(btnStart);
-
-//   toggleBtnStart(btnStart);
-//   events.quitBtn(btnStart);
-// attach event listener to button quit
-// detach from event target the listener.
-// come back to initial state.
 
 const startGame = (body) => {
-  console.log(body);
   const btnStart = body.querySelector('#body-main #controllers #button-start');
-  console.log(btnStart);
   btnStart.addEventListener('click', startGameLoop);
 };
 
 const startGameLoop = (event) => {
-  console.log(game);
   const newGame = game.init();
-  console.log(event.target);
+  game.placement(newGame);
   game.playGame(newGame);
   startGameRemove(event.target);
   DOM().toggleBtnStart(event.target);
   quitGame(event.target);
-  console.log('press');
 };
 
 const startGameRemove = (btn) => {
   btn.removeEventListener('click', startGameLoop);
 };
+
 /**
  * Add click listener to cpu board.
  * @param {HTMLElement} table
@@ -62,17 +51,10 @@ const quitGame = (btn) => {
 const quitGameLoop = (event) => {
   const body = document.querySelector('#hook');
   const newGameInit = game.init();
-  const newGamePlacement = game.placement();
+  const newGamePlacement = game.placement(newGameInit);
 
-  console.log(game);
-  console.log(event.target);
   removeQuitGame(event.target);
-  console.log(newGame);
   DOM().toggleBtnStart(event.target);
-  // render non ha senso. troppo, render solo delle board per iniziare poi
-  // vediamo e cancellare le board prima
-  // bisogna resettare game e passarlo al game loop
-  // bisogna resettare gli shot per valid attack
   const main = body.querySelector('#body-main');
 
   DOM().removeGameContent(main);
@@ -80,14 +62,12 @@ const quitGameLoop = (event) => {
   DOM().renderGameContent(main, newGameInit, newGamePlacement);
   startGame(body);
   DOM().setMessage('Place your ships');
-  //event.target.addEventListener('click', startGameLoop);
-
-  //game.reset();
 };
 
 const removeQuitGame = (btn) => {
   btn.removeEventListener('click', quitGameLoop);
 };
+
 /**
  * Export event listeners
  * @param {HTMLElement} body
@@ -108,6 +88,4 @@ export default eventListeners;
  * - Functional review
  * - Check test.
  * - TOP forward
- *
- *
  * */
