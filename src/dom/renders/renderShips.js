7f5c24d6bf2a5c565e728abfd1c8f3774dcdd6b1
Daniele Campari <danielecampari@gmail.com>
2023-08-21 14:58:51 +0200

    render ships and icons

---
+++ src/dom/renders/renderShips.js
@@ -0,0 +1,70 @@
+import createAndRenderElement from '../domUtil';
+
+/**
+ * Render container for player ship icon.
+ * @param {Node} section
+ * @returns
+ */
+const renderShipsPlayerContainer = (section) =>
+  createAndRenderElement(
+    'div',
+    { id: 'ships-player', class: 'ships-container player-ships' },
+    null,
+    section
+  );
+
+/**
+ * Render container for cpu ship icon.
+ * @param {Node} section
+ * @returns
+ */
+const renderShipsRivalContainer = (section) =>
+  createAndRenderElement(
+    'div',
+    { id: 'ships-rival', class: 'ships-container rival-ships' },
+    null,
+    section
+  );
+
+/**
+ * Render icon for ships
+ * @param {Node} section
+ * @param {Object} ships
+ */
+const renderShipIcons = (section, ships) => {
+  Object.entries(ships).forEach(([ship, descriptions]) => {
+    const div = createAndRenderElement('div', { id: ship }, null, section);
+    descriptions.forEach((description) => {
+      createAndRenderElement(
+        'img',
+        { src: description.icon, alt: `${ship}-icon`, class: 'icons-size' },
+        null,
+        div
+      );
+    });
+  });
+};
+
+/**
+ * Render the ship on board
+ * @param {Array[]} board
+ */
+const renderPlayerShips = (board, table) => {
+  const rows = Array.from(table.rows);
+  rows.forEach((row, rowIndex) => {
+    const cells = Array.from(row.cells);
+    cells.forEach((td, colIndex) => {
+      const cell = board[rowIndex][colIndex];
+      if (cell !== null) {
+        td.classList.add('ship-placed');
+      }
+    });
+  });
+};
+
+export {
+  renderShipsPlayerContainer,
+  renderShipsRivalContainer,
+  renderShipIcons,
+  renderPlayerShips
+};
