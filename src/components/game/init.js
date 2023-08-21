3be188ff5463f4a9eff0dff32541bd6aefdaf2df
Daniele Campari <danielecampari@gmail.com>
2023-08-21 15:00:29 +0200

    separate placement from init

--- src/components/game/init.js
+++ src/components/game/init.js
@@ -1,10 +1,5 @@
 import Player from '../players';
 import Gameboard from '../gameBoard';
-import Ship from '../ships';
-import iconCarrier from '../../assets/icons/carrier.svg';
-import iconBattleship from '../../assets/icons/battleship.svg';
-import iconSubmarine from '../../assets/icons/submarine.svg';
-import iconDestroyer from '../../assets/icons/destroyer.svg';
 
 /**
  * Create a player.
@@ -39,41 +34,4 @@ const createNewGameboards = () => ({
   playerTwoGameboard: Gameboard()
 });
 
-/**
- * Create ships
- * @param {Object} type - Type of ship
- * @returns An Object with ships
- */
-const createShips = (type) =>
-  Array.from({ length: type.number }, () => ({
-    body: Ship(type.size),
-    size: type.size,
-    icon: type.icon
-  }));
-
-/**
- * Describe ships
- */
-const shipTypes = {
-  carrier: { number: 1, size: 5, icon: iconCarrier },
-  battleships: { number: 2, size: 4, icon: iconBattleship },
-  submarines: { number: 3, size: 3, icon: iconSubmarine },
-  destroyers: { number: 4, size: 2, icon: iconDestroyer }
-};
-
-/**
- * Create ships for each player
- * @returns Object with ships
- */
-const createShipPlayers = () =>
-  Object.keys(shipTypes).reduce((ships, type) => {
-    ships[type] = createShips(shipTypes[type]);
-    return ships;
-  }, {});
-
-const createPlayersShips = () => ({
-  playerOneShips: createShipPlayers(),
-  playerTwoShips: createShipPlayers()
-});
-
-export { createNewPlayers, createNewGameboards, createPlayersShips };
+export { createNewPlayers, createNewGameboards };
