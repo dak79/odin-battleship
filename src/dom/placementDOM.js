939fe85ba2b43975247b72825b722ef6dfffb03d
Daniele Campari <danielecampari@gmail.com>
2023-08-22 18:01:16 +0200

    add placement fase

--- src/dom/placementDOM.js
+++ src/dom/placementDOM.js
@@ -1,35 +1,131 @@
-import {
-  renderShipsPlayerContainer,
-  renderShipsRivalContainer,
-  renderShipIcons,
-  renderPlayerShips
-} from './renders/renderShips';
+import { renderShipsRivalContainer } from './renders/renderShips';
+import eventListeners from './eventListeners';
+import createAndRenderElement from './domUtil';
+
+/**
+ * Render the ship on board
+ * @param {Array[]} board
+ */
+const renderPlayerShips = (board, table) => {
+  const rows = Array.from(table.rows);
+  rows.forEach((row, rowIndex) => {
+    const cells = Array.from(row.cells);
+    cells.forEach((td, colIndex) => {
+      const cell = board[rowIndex][colIndex];
+      if (cell !== null) {
+        td.classList.add('ship-placed');
+      }
+    });
+  });
+};
+
+/**
+ * Render container for player ship icon.
+ * @param {Node} section
+ * @returns
+ */
+const renderShipsPlayerContainer = (section) =>
+  createAndRenderElement(
+    'div',
+    { id: 'ships-player', class: 'ships-container player-ships' },
+    null,
+    section
+  );
+
+/**
+ * Render icon for ships
+ * @param {Node} section
+ * @param {Object} ships
+ */
+const renderShipIcons = (section, ship) => {
+  if (!section.querySelector(`#${ship.body.init.type}`)) {
+    createAndRenderElement('div', { id: ship.body.init.type }, null, section);
+  }
+
+  const div = section.querySelector(`#${ship.body.init.type}`);
+
+  createAndRenderElement(
+    'img',
+    { src: ship.icon, alt: `${ship.body.init.type} icon`, class: 'icons-size' },
+    null,
+    div
+  );
+};
 
 const renderShipSummary = (parent, ships) => {
-  const shipsContainerPlayer = renderShipsPlayerContainer(parent);
-  renderShipIcons(shipsContainerPlayer, ships.playerOneShips);
+  // const shipsContainerPlayer = renderShipsPlayerContainer(parent);
+  // renderShipIcons(shipsContainerPlayer, ships.playerOneShips);
 
   const shipsContainerRival = renderShipsRivalContainer(parent);
   renderShipIcons(shipsContainerRival, ships.playerTwoShips);
 };
 
-const renderShipsOnBoard = (board) => {
-  const tablePlayer = document.querySelector('#board-player-table');
-  renderPlayerShips(board.playerOneGameboard.board, tablePlayer);
-};
-
 const removeShipsSummary = (parent) => {
   const elements = [
-    parent.querySelector('#ships-player'),
-    parent.querySelector('#ships-rival')
+    parent.querySelector('#ships-player')
+    //parent.querySelector('#ships-rival')
   ];
 
   elements.forEach((element) => element.remove());
 };
 
+/**
+ * Render placement on boards
+ * @param {Object} attacker
+ * @param {Object} opponent
+ * @param {Boolean} isPlayerOne
+ */
+const renderPlacement = async (ships, gameboard) => {
+  const events = eventListeners();
+  const body = document.querySelector('#hook');
+  const iconContainer = body.querySelector('#body-main #ships-player');
+  const tablePlayer = body.querySelector(
+    '#body-main #board-player #board-player-table'
+  );
+
+  const arrShips = Object.values(ships);
+  for (const shipType of arrShips) {
+    for (const ship of shipType) {
+      let validPlacement = false;
+      while (!validPlacement) {
+        const coord = await events.addClicks(body, true);
+        const [row, col] = coord;
+        const dir = true;
+        validPlacement = gameboard.placeShip(
+          gameboard.board,
+          ship.body,
+          parseInt(row),
+          parseInt(col),
+          dir
+        );
+      }
+      renderPlayerShips(gameboard.board, tablePlayer);
+      renderShipIcons(iconContainer, ship);
+    }
+  }
+};
+
+/*
+ * PL1:
+ *   - Add button rotate
+ *   - Add ship shadow on hover
+ *   - Shadow veritcal and horizonatal
+ *   - Add move with drag and drop
+ *   - Fix the message for player 1
+ * PL2:
+ *   - Random placement
+ *   - Render icons container
+ *   - Render icons
+ * Pass to game.loop:
+ *  -  Check button quit
+ */
+
 const placementDOM = () => ({
   renderShipSummary: (parent, ships) => renderShipSummary(parent, ships),
-  renderShipsOnBoard: (board) => renderShipsOnBoard(board),
+  renderShipsPlayerContainer: (main) => renderShipsPlayerContainer(main),
+  renderPlacement: async (ships, gameboard) =>
+    renderPlacement(ships, gameboard),
+  //renderShipsOnBoard: (board) => renderShipsOnBoard(board),
   removeShipsSummary: (parent) => removeShipsSummary(parent)
 });
 
